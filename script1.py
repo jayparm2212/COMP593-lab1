df9a92d439e45df60bc39559402e33f217d42994
jayparm2212 <156491201+jayparm2212@users.noreply.github.com>
2024-01-16 11:32:15 -0500

    Modified both files in the repo clone
    -deleted a print(0 statement from script1.py
    -Added two print() statement to script2.py

--- script1.py
+++ script1.py
@@ -1,3 +1,2 @@
 # print lines from Zen of python
 print("It is a first lab.")
-print("it is easy lab.")
--- script2.py
+++ script2.py
@@ -1,3 +1,4 @@
 # lab 1 is easy just follow the instruction.
 # lab 1 is easy just follow the instruction.
 print("God id great")
+print("Morrison is a clever boy.")
